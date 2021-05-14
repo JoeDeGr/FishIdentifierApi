e04c6240e4cbddb020f2f61d3e34d60d5766d09f
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-14 09:13:35 -0400

    added has_secure_password and expected initial validations to user model --quoted out until routes are established.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    # has_secure_password
+    # validates :username, presence: true
+    # validates :email, presence: true
+    # validates :email, uniqueness: true 
+
     has_many :users_specy
     has_many :species, through: :users_specy
     has_many :users_genu
