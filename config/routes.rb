0b7ff14ef6d572f930569e9a682c503c91d769e8
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-14 14:08:22 -0400

    working on users in router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'genus/show'
   get 'species/show'
-  post 'users/create'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
+  # post 'users/create'
+  # get 'users/show'
+  # patch 'users/id', to: 'users#update'
+  # get 'users/destroy'
+
+  resources :users, only: [:create, :show, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
