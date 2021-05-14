ab74323e6b2d1414938c0391865c112fe0db8fd6
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-14 09:09:40 -0400

    added require_login and current_user to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    # def require_login
+    #     binding.pry
+    #       return head(:forbidden) unless session.include? :user_id
+    #   end
+    
+    #   def current_user
+    #       user = (User.find(session[:user_id]) || User.new)
+    #   end
 end
