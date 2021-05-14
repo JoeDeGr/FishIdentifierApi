0eb08774337043ee5cb159006379e9ba00541c9a
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-14 09:27:14 -0400

    created users routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/show'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
