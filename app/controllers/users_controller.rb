c447343d8979f2513f6925de49bbf74727032b6d
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-14 10:07:25 -0400

    working on Users_Controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def create
+    user = User.find_by(user_params) || User.new(user_params)
   end
 
   def show
@@ -9,5 +10,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
