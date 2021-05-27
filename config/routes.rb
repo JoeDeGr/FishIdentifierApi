33a611ab297456605d3939990cbcc1031436f581
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-27 15:08:46 -0400

    Cleaning up auth connections.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
   get 'genus/show'
   get 'species/show'
-  # post 'users/create'
-  # get 'users/show'
-  # patch 'users/id', to: 'users#update'
-  # get 'users/destroy'
-
-  resources :users, only: [:create, :show, :update, :destroy]
-
+  get "/auto_login", to: "auth#auto_login"
+  get "/authorized_user", to: "auth#authorized_user"
   post "/login", to: "auth#login"
 
-  get "/auto_login", to: "auth#auto_login"
-
-  get "/authorized_user", to: "auth#authorized_user"
+  resources :users, only: [:create, :show, :update, :destroy]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
